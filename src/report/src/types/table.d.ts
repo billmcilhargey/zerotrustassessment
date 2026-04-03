bbcd2c32688b7bc992f9fb630e00ce2984a599e7
Dr Bill Mcilhargey <19168174+billmcilhargey@users.noreply.github.com>
2026-04-03 20:11:25 +0000

    Remove unused pages and configuration files from the report module

--- src/report/src/types/table.d.ts
+++
@@ -1,8 +0,0 @@
-import { ColumnMeta } from "@tanstack/table-core";
-
-// Extend or augment the `.meta` interface
-declare module "@tanstack/table-core" {
-  interface ColumnMeta<TData, TValue> {
-    label?: string;
-  }
-}
